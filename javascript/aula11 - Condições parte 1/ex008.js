0275054a23352756c2798641e1fbd8046c6cdf72
Mateus <f.mateusaraujo20@gmail.com>
2022-07-23 15:01:58 -0300

    add if and model ex

---
+++ javascript/aula11 - Condições parte 1/ex008.js
@@ -0,0 +1,8 @@
+var vel = 65.5
+console.log('A velocidade do seu carro é de ' + vel)
+
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade');
+} else {
+    console.log('Velocidade dentro do permitido');
+}
---
+++ javascript/aula12 - Condições parte 2/ex012.js
@@ -0,0 +1,4 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minuto = agora.getMinutes()
+console.log(`Agora são ${hora} horas e ${minuto} minutos`);
