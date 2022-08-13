78cef71d73966b398929491395a532622c46fe58
Mateus <f.mateusaraujo20@gmail.com>
2022-08-13 12:20:44 -0300

    add var comp

---
+++ javascript/aula15 - Variáveis Compostas/ambiente.js
@@ -0,0 +1,11 @@
+let num = [2, 5, 6, 2, 4, 0]
+
+console.log(`vetor: ${num}`)
+console.log(num)
+
+num[6] = 7
+//adiciona 7 ao final do array
+
+console.log(num)
+
+num.push(8) //outra maneira de adicionar número ao final do array, sem saber a posição
